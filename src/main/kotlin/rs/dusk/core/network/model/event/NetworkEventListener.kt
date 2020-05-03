2e5a1de1a1455d853e680758554e690ed9667e04
kiairatech <contact@kiaira.tech>
2020-05-02 22:03:49 -0400

    Remove old network event system

--- src/main/kotlin/rs/dusk/core/network/model/event/NetworkEventListener.kt
+++
@@ -1,8 +0,0 @@
-package rs.dusk.core.network.model.event
-
-/**
- * @author Tyluur <contact@kiaira.tech>
- * @since April 08, 2020
- */
-class NetworkEventListener {
-}
--- src/main/kotlin/rs/dusk/core/network/model/event/type/ActivityNetworkEvent.kt
+++
@@ -1,9 +0,0 @@
-package rs.dusk.core.network.model.event.type
-
-import rs.dusk.core.network.model.event.NetworkEvent
-
-/**
- * @author Tyluur <contact@kiaira.tech>
- * @since April 08, 2020
- */
-data class ActivityNetworkEvent(val active: Boolean) : NetworkEvent
