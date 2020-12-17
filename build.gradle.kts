46a86ac4a4f9246ec946e9f203546503ba275b98
Tyluur <itstyluur@gmail.com>
2020-12-17 11:52:02 -0500

    upgrade to v0.1.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,9 +21,8 @@ allprojects {
     apply(plugin = "idea")
     apply(plugin = "org.jetbrains.kotlin.jvm")
 
-
     group = "rs.dusk.core"
-    version = "0.1.2"
+    version = "0.1.3"
 
     java.sourceCompatibility = JavaVersion.VERSION_11
 
