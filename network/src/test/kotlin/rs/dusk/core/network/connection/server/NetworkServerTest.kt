034b7bd40d9ade6a65287283b50b695a46d64367
Tyluur <itstyluur@gmail.com>
2020-12-17 10:11:31 -0500

    start on the tests for the network server

--- network/src/test/kotlin/rs/dusk/core/network/connection/server/NetworkServerTest.kt
+++ network/src/test/kotlin/rs/dusk/core/network/connection/server/NetworkServerTest.kt
@@ -1,30 +1,42 @@
 package rs.dusk.core.network.connection.server
 
+import rs.dusk.core.network.codec.message.MessageReader
+import rs.dusk.core.network.connection.ConnectionFactory
+import rs.dusk.core.network.connection.ConnectionPipeline
+import rs.dusk.core.network.connection.ConnectionSettings
+import rs.dusk.core.network.connection.event.ChannelEventChain
+import rs.dusk.core.network.connection.event.ChannelEventListener
+import rs.dusk.core.network.security.SslConfig
+import rs.dusk.core.network.security.SslServerInitializer
+
 /**
  * @author Tyluur <contact@kiaira.tech>
  * @since March 18, 2020
  */
-class NetworkServerTest
-
-/*
-fun main() {
+class NetworkServerTest {
 	val factory = ServerConnectionFactory()
 	
 	val settings = ConnectionSettings("localhost", 43593)
-	val server = NetworkServer(settings)
-	
-	*/
-/*
-	val config = SslConfig("./", "", "") ?: throw IllegalStateException("Unable to create ssl configuration")
-	val sslInitializer = SslServerInitializer(config) // TODO
-	*//*
 
+	val config = SslConfig("./", "", "") ?: throw IllegalStateException("Unable to create ssl configuration")
+	val sslInitializer = SslServerInitializer(config) // TODO - finish implementing this
 	
 	val chain = ChannelEventChain()
 	
-	val pipeline = ConnectionPipeline {
-		it.addLast("message.handler", MessageReader())
-		it.addLast("connection.listener", ChannelEventListener(chain))
+	fun bind() {
+		val pipeline = ConnectionPipeline {
+			it.addLast("message.handler", MessageReader())
+			it.addLast("connection.listener", ChannelEventListener(chain))
+		}
+		
 	}
-	factory.bind(server, chain, pipeline)
-}*/
+}
+
+class ServerConnectionFactory : ConnectionFactory() {
+
+}
+
+fun main() {
+	
+	val server = NetworkServerTest()
+}
