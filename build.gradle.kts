0c91230fcb5a554ec1337f108428161e6737b4f5
Tyluur <itstyluur@icloud.com>
2020-12-26 01:37:27 -0500

    update with new build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,128 +1,193 @@
-import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
-import org.jetbrains.dokka.gradle.DokkaTask
-import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
-import org.jetbrains.kotlin.gradle.plugin.KotlinMultiplatformPluginWrapper
+version = "1"
 
-val ossrhUsername: String? by ext
-val ossrhPassword: String? by ext
-
-description = "#1 rsps. no noobs allowed"
+val duskCoreVersion = "1.0.0"
+val javaVer = JavaVersion.VERSION_15
+val koinVersion = "2.1.5"
+val junitVersion = "5.6.2"
+val exposedVersion = "0.24.1"
+val jacksonVersion = "2.11.0"
 
 plugins {
-	base
-	id("java")
-	id()
-	id("com.github.ben-manes.versions") version Versions.versionsPlugin
+	kotlin("jvm") version "1.4.21"
+	application
 	
-	kotlin("multiplatform") version Versions.kotlin apply false
-	id("kotlinx.benchmark") version Versions.kotlinBenchmark apply false
-	id("org.jetbrains.dokka") version Versions.dokka apply false
-	id("org.jetbrains.kotlin.plugin.allopen") version Versions.kotlin apply false
+	`java-library`
+	`maven-publish`
+	maven
+	java
+	signing
+	
+	base
 }
 
-tasks.withType<DependencyUpdatesTask> {
-	rejectVersionIf {
-		listOf("alpha", "beta", "rc", "cr", "m", "eap", "pr", "dev").any {
-			candidate.version.contains(it, ignoreCase = true)
-		}
+application {
+	mainClassName = "BootstrapKt"
+}
+
+buildscript {
+	repositories {
+		mavenCentral()
+		google()
+		jcenter()
 	}
 }
 
 allprojects {
+	apply(plugin = "idea")
+	apply(plugin = "org.jetbrains.kotlin.jvm")
+	
 	repositories {
 		mavenCentral()
 		jcenter()
 		maven("https://dl.bintray.com/kotlin/kotlinx")
-	}
-}
-
-subprojects {
-	plugins.withType<MavenPublishPlugin> {
-		apply(plugin = "org.gradle.signing")
-		
-		plugins.withType<KotlinMultiplatformPluginWrapper> {
-			apply(plugin = "org.jetbrains.dokka")
-			
-			val dokka by tasks.existing(DokkaTask::class) {
-				outputFormat = "javadoc"
-				outputDirectory = "$buildDir/docs/javadoc"
-			}
-			
-			val javadocJar by tasks.registering(Jar::class) {
-				group = LifecycleBasePlugin.BUILD_GROUP
-				description = "Assembles a jar archive containing the Javadoc API documentation."
-				archiveClassifier.set("javadoc")
-				dependsOn(dokka)
-				from(dokka.get().outputDirectory)
-			}
-			
-			configure<KotlinMultiplatformExtension> {
-				explicitApi()
-				
-				jvm {
-					mavenPublication {
-						artifact(javadocJar.get())
-					}
-				}
-				
-				js {
-					browser()
-					nodejs()
-				}
-			}
+		maven {
+			// change URLs to point to your repos, e.g. http://my.org/repo
+			val releasesRepoUrl = uri("$buildDir/repos/releases")
+			val snapshotsRepoUrl = uri("$buildDir/repos/snapshots")
+			url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
 		}
+	}
+	
+	dependencies {
+		//Main
+		implementation(group = "rs.dusk.core", name = "network", version = "$duskCoreVersion")
+		implementation(group = "rs.dusk.core", name = "utility", version = "$duskCoreVersion")
 		
-		configure<PublishingExtension> {
-			repositories {
-				maven {
-					if (project.version.toString().endsWith("SNAPSHOT")) {
-						setUrl("https://oss.sonatype.org/content/repositories/snapshots")
-					} else {
-						setUrl("https://oss.sonatype.org/service/local/staging/deploy/maven2")
-					}
-					
-					credentials {
-						username = ossrhUsername
-						password = ossrhPassword
-					}
+		implementation(kotlin("stdlib-jdk8"))
+		implementation(kotlin("reflect"))
+		implementation("io.netty:netty-all:4.1.44.Final")
+		implementation(group = "com.displee", name = "rs-cache-library", version = "6.4")
+		implementation(group = "org.yaml", name = "snakeyaml", version = "1.26")
+		implementation(group = "io.github.classgraph", name = "classgraph", version = "4.8.78")
+		implementation(
+			group = "com.michael-bull.kotlin-inline-logger",
+			name = "kotlin-inline-logger-jvm",
+			version = "1.0.2"
+		)
+		implementation(group = "org.koin", name = "koin-core", version = koinVersion)
+		implementation(group = "org.koin", name = "koin-logger-slf4j", version = koinVersion)
+		implementation(group = "org.jetbrains.kotlinx", name = "kotlinx-coroutines-core", version = "1.3.7")
+		
+		//Logging
+		implementation("org.slf4j:slf4j-api:1.7.30")
+		implementation("ch.qos.logback:logback-classic:1.2.3")
+		
+		//Utilities
+		implementation("com.google.guava:guava:29.0-jre")
+		implementation("org.apache.commons:commons-lang3:3.10")
+		implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+		implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+		implementation("com.fasterxml.jackson.core:jackson-core:$jacksonVersion")
+		implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
+		implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:$jacksonVersion")
+		implementation("org.postgresql:postgresql:42.2.12")
+		implementation("com.zaxxer:HikariCP:3.4.5")
+		implementation("it.unimi.dsi:fastutil:8.3.1")
+		
+		//Testing
+		testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+		testImplementation("org.junit.jupiter:junit-jupiter-params:$junitVersion")
+		testImplementation(group = "org.koin", name = "koin-test", version = koinVersion)
+		testImplementation(group = "io.mockk", name = "mockk", version = "1.10.0")
+	}
+	
+	tasks {
+		compileKotlin {
+			kotlinOptions.jvmTarget = "1.8"
+		}
+		compileTestKotlin {
+			kotlinOptions.jvmTarget = "1.8"
+		}
+	}
+	
+	tasks {
+		task("generatePom") {
+			doLast {
+				maven.conf2ScopeMappings.apply {
+					addMapping(
+						0, configurations.getByName("implementation"),
+						org.gradle.api.artifacts.maven.Conf2ScopeMappingContainer.COMPILE
+					)
+					addMapping(
+						0, configurations.getByName("compileOnly"),
+						org.gradle.api.artifacts.maven.Conf2ScopeMappingContainer.PROVIDED
+					)
 				}
-			}
-			
-			publications.withType<MavenPublication> {
-				pom {
-					name.set(project.name)
-					url.set("https://github.com/dusk-rs/dusk-core")
-					inceptionYear.set("2020)
-					
-					developers {
-						developer {
-							name.set("Tyluur")
-							url.set("dusk.rs/tyluur")
+				maven.pom {
+					withGroovyBuilder {
+						"project" {
+							"build" {
+								"plugins" {
+									"plugin" {
+										setProperty("artifactId", "maven-compiler-plugin")
+										setProperty("version", "3.8.1")
+										"configuration" {
+											setProperty("compilerVersion", "$javaVer")
+											setProperty("source", "$javaVer")
+											setProperty("target", "$javaVer")
+										}
+									}
+								}
+							}
 						}
 					}
-					
-					scm {
-						connection.set("scm:git:https://github.com/dusk-rs/dusk-core")
-						developerConnection.set("scm:git:git@github.com:dusk-rs/dusk-core.git")
-						url.set("https://github.com/dusk-rs/dusk-core")
-					}
-					
-					issueManagement {
-						system.set("GitHub")
-						url.set("https://github.com/dusk-rs/dusk-core/issues")
-					}
-					
-					ciManagement {
-						system.set("GitHub")
-						url.set("https://github.com/dusk-rs/dusk-core/actions?query=workflow%3Aci")
-					}
-				}
-			}
-			
-			configure<SigningExtension> {
-				useGpgCmd()
-				sign(publications)
+				}.writeTo("pom.xml")
 			}
 		}
 	}
+	
+	publishing {
+		publications {
+			create<MavenPublication>("mavenJava") {
+				artifactId = "my-library"
+				from(components["java"])
+				versionMapping {
+					usage("java-api") {
+						fromResolutionOf("runtimeClasspath")
+					}
+					usage("java-runtime") {
+						fromResolutionResult()
+					}
+				}
+				pom {
+					name.set("Dusk")
+					description.set("A concise description of my library")
+					url.set("http://www.dusk.rs")
+					properties.set(
+						mapOf(
+							"1337" to "true",
+							"u" to "noob"
+						)
+					)
+					licenses {
+						license {
+							name.set("The Apache License, Version 2.0")
+							url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+						}
+					}
+					developers {
+						developer {
+							id.set("tyluur")
+							email.set("itstyluur@violated.rs")
+						}
+					}
+					scm {
+						connection.set("scm:git:git://dusk.rs/dusk.git.git")
+						developerConnection.set("scm:git:ssh://github.com/dusk-rs/dusk.git")
+						url.set("http://github.com/dusk-rs/dusk")
+					}
+				}
+			}
+		}
+	}
+	
+	base.archivesBaseName = "gradle"
+	
+	val webAssetPatterns = Action<CopySpec> {
+		include("**/*.html", "**/*.png", "**/*.jpg")
+	}
+	
 }
+
+group = "dusk.rs"
+description = "#1 rsps. no noobs allowed"
